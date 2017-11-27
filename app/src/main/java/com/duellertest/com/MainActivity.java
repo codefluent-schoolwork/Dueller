8cc3ec5d6edcd224606b86e2318ec90647f5e237
CodeFluent <wasfimomenwim@gmail.com>
2017-11-26 22:42:21 -0500

    all first part setup complete, beginning firebase integration

--- app/src/main/java/com/duellertest/com/MainActivity.java
+++ app/src/main/java/com/duellertest/com/MainActivity.java
@@ -2,6 +2,7 @@ package com.duellertest.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            return true;
+        }
+
+        return false;
     }
 }
