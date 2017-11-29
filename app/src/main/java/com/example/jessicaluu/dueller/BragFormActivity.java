579f41d94d42db906c891d48ac2b689c061229e0
Jessica <jessicaluu@github.com>
2017-11-29 15:21:15 -0500

    TESTING - duel details view - bragging claim view

---
+++ app/src/main/java/com/example/jessicaluu/dueller/BragFormActivity.java
@@ -0,0 +1,13 @@
+package com.example.jessicaluu.dueller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BragFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brag_form);
+    }
+}
