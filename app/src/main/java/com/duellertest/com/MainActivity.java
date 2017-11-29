f90e76c51be814a9aee02b43a0a98869e871eecd
CodeFluent <wasfimomenwim@gmail.com>
2017-11-29 13:17:33 -0500

    added login views

--- app/src/main/java/com/duellertest/com/MainActivity.java
+++ app/src/main/java/com/duellertest/com/MainActivity.java
@@ -1,15 +1,29 @@
 package com.duellertest.com;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        if (mFirebaseUser == null) {
+            loadLogInView();
+        }
     }
 
     @Override
@@ -25,5 +39,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return false;
+    }
+
+    private void loadLogInView() {
+        Intent intent = new Intent(this, LogInActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
