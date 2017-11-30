6c44973731e441e669475cfb5d4b281af5ff85f0
CodeFluent <wasfimomenwim@gmail.com>
2017-11-30 09:41:05 -0500

    got log on db

--- app/src/main/java/com/duellertest/com/MainActivity.java
+++ app/src/main/java/com/duellertest/com/MainActivity.java
@@ -1,17 +1,32 @@
 package com.duellertest.com;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = "MainActivity";
+
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Query mQuery;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,21 @@ public class MainActivity extends AppCompatActivity {
         if (mFirebaseUser == null) {
             loadLogInView();
         }
+
+
+        db.collection("duel")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for(DocumentSnapshot documentSnapshot: task.getResult()) {
+                                Log.d(TAG, documentSnapshot.getId() + " => " + documentSnapshot.getData());
+
+                            }
+                        }
+                    }
+                });
     }
 
     @Override
